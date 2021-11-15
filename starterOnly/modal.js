5e6f8daa6f0da9f4164ae9133a131cb7b5588b9e
Johnny Corvisier <johnny.corvisier.lc@gmail.com>
2021-11-15 07:11:59 +0100

    Form working as intended and responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,18 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const inputs = document.querySelectorAll(".text-control");
+const firstNameInput = inputs[0];
+const surnameInput = inputs[1];
+const emailInput = inputs[2];
 const modalClose = document.querySelectorAll(".close");
-const submitBtn = document.querySelector(".btn-submit");
 const formCheck = document.querySelector("#checkbox1");
-const nameErr = document.querySelector("#first");
-const lastNameErr = document.querySelector("#last");
-const emailErr = document.querySelector("#email");
 const quantity = document.querySelector("#quantity");
-const validCity = document.querySelector("#valid-city");
-const errStar = document.querySelector("#required-star");
 const checkCondition = document.querySelector("#required-check");
-const formIsValid = document.querySelector("#valid-form");
+const submitBtn = document.querySelector("#submit-btn")
+const formIsValid = document.querySelector(".valid-form");
 const signUp = document.querySelector("#btn-signup");
+const regexEmail = /.+@.+..+/;
+const regexNames = /[a-zA-Z]{2,}/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -42,8 +43,6 @@ function closeModal() {
 
 // display confirmation message on submit
 
-submitBtn.addEventListener("submit", noReload);
-
 function validForm() {
   formIsValid.style.display = "flex";
 }
@@ -52,68 +51,38 @@ function hideSignup() {
   signUp.style.display = "none";
 }
 
-// check if conditions true
-
+// Valid all inputs in the form
 function validate() {
-  if (formCheck.checked === false) {
-    checkCondition.style.display = "flex";
-    errStar.style.display = "inline-block";
-    return false;
-  }
-  return true;
-}
+  const resultName = regexNames.test(firstNameInput.value);
+  const resultSurname = regexNames.test(surnameInput.value);
+  const resultEmail = regexEmail.test(emailInput.value);
+  const birthDate = !!(Date.parse(inputs[3].value));
 
-
-//function that check if at least one radio button is selected
-
-function check() {
-  if (quantity.value == 0) {
-    return true;
-  }
-  var radios = document.getElementsByName("location");
-  for (var i = 0, len = radios.length; i < len; i++) {
-    if (radios[i].checked) {
-      return true;
+  var hasLocation = false;
+  if (quantity.value > 0) {    
+    var radios = document.getElementsByName("location");
+    for (var i = 0, len = radios.length; i < len; i++) {
+      if (radios[i].checked) {
+        hasLocation = true;
+        break;
+      }
     }
   }
-  validCity.style.display = "flex";
+  const locationChecked = quantity.value == 0 || hasLocation;
+  const rules = formCheck.checked;
+
+  formData[0].setAttribute('data-error-visible', !resultName);
+  formData[1].setAttribute('data-error-visible', !resultSurname);
+  formData[2].setAttribute('data-error-visible', !resultEmail);
+  formData[3].setAttribute('data-error-visible', !birthDate);
+  formData[5].setAttribute('data-error-visible', !locationChecked);
+  formData[6].setAttribute('data-error-visible', !rules);
+
+  if(resultEmail && resultName && resultSurname && birthDate && locationChecked && rules) 
+  {  
+    validForm();
+    hideSignup();
+    closeModal();
+  }
   return false;
 }
-
-function noReload(event) {
-    event.preventDefault();
-}
-
-//appropriate error messages
-
-nameErr.addEventListener('input', _e => {
-  if (nameErr.validity.patternMismatch) {
-    nameErr.setCustomValidity('Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
-  } else {
-    nameErr.setCustomValidity('');
-  }
-})
-
-lastNameErr.addEventListener('input', _e => {
-  if (lastNameErr.validity.patternMismatch) {
-    lastNameErr.setCustomValidity('Veuillez entrer 2 caractères ou plus pour le champ du nom.');
-  } else {
-    lastNameErr.setCustomValidity('');
-  }
-})
-
-emailErr.addEventListener('input', _e => {
-  if (emailErr.validity.patternMismatch) {
-    emailErr.setCustomValidity("L'email saisi n'est pas valide (exemple@exemple.com).");
-  } else {
-    emailErr.setCustomValidity('');
-  }
-})
-
-quantity.addEventListener('input', _e => {
-  if (quantity.validity.patternMismatch) {
-    quantity.setCustomValidity("Veuillez entrer un chiffre.");
-  } else {
-    quantity.setCustomValidity('');
-  }
-})
